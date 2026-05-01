ff9b9c967232ce686aadca711070150fe0c0ba3b
elizabethb90 <elizabethb90@kprschools.ca>
2026-05-01 13:51:42 -0400

    h

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
 # Bubble Sort
 array1 = [1, 3, 4, 7, 5, 10, 9, 8, 6]
 array2 = [1, 6, 4, 7, 5, 10, 9, 8, 5]
-def bubble_sort(array1):
-    n = len(array1)
+def bubble_sort(array):
+    n = len(array)
     for i in range(n):
         swapped = False
         for j in range(0, n - i - 1):
-            if array1[j] > array1[j + 1]:
-                array1[j], array1[j + 1] = array1[j + 1], array1[j]
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
                 swapped = True
         if not swapped:
             break
     return array1
 
-# Merge Sort
-def merge_sort(array2):
-    if len(array2) <= 1:
-        return array2
+bubble_sort(array1)
+bubble_sort(array2)
 
-    mid = len(array2) // 2
-    left = merge_sort(array2[:mid])
-    right = merge_sort(array2[mid:])
+# Merge Sort
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+
+    mid = len(array) // 2
+    left = merge_sort(array[:mid])
+    right = merge_sort(array[mid:])
 
     return merge(left, right)
+
+merge_sort(array1)
+merge_sort(array2)
 
 def merge(left, right):
     result = []
